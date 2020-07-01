775582e10cbc4be7c6768df2aaf4601e2cb7b15c
Dmitriy Sosnovskiy <45599375+DmitriySosnovskiy@users.noreply.github.com>
2020-07-01 22:03:20 +0600

    Initial commit

--- DijkstraAlgorithm/src/Main.kt
+++ DijkstraAlgorithm/src/Main.kt
@@ -1,5 +1,23 @@
-package main
+import javafx.application.Application
+import javafx.fxml.FXMLLoader.load
+import javafx.scene.Parent
+import javafx.scene.Scene
+import javafx.stage.Stage
 
-fun main(args : Array<String>) {
-    println("Hello, World!")
+class Main : Application() {
+
+    val layout = "/res/layout.fxml"
+
+    override fun start(primaryStage: Stage?) {
+        System.setProperty("prism.lcdtext", "false") // for beautiful fonts on linux
+        primaryStage?.scene = Scene(load<Parent?>(Main.javaClass.getResource(layout)))
+        primaryStage?.show()
+    }
+
+    companion object {
+        @JvmStatic
+        fun main(args: Array<String>) {
+            launch(Main::class.java)
+        }
+    }
 }
