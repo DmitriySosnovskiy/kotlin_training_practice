00d74781a626e083238ec92105bf8b09c0309b23
garet2gis <br-avm32@yandex.ru>
2020-07-06 19:18:36 +0300

    Added methods for algorithm in mainpresenter

--- DijkstraAlgorithm/src/main/kotlin/presenters/MainPresenter.kt
+++ DijkstraAlgorithm/src/main/kotlin/presenters/MainPresenter.kt
@@ -1,34 +1,65 @@
 package presenters
 
 import models.Edge
+import models.Graph
 import models.SnapshotKeeper
+import models.Snapshot
 import views.graphview.UIEdge
 import views.graphview.UINode
 
-interface ViewObserver{
-    fun update()
+interface ToolBarView{
+    fun HandleToolBar()
+}
+
+class DijkstraAlgorithmController(){
+    var snapshotKeeper : SnapshotKeeper = SnapshotKeeper()
+    var startNode : Int = -1
+    var endNode : Int = -1
+    var currentStep : Int = 0
+
+    fun initStart(startNode:Int, endNode:Int, snapshots : SnapshotKeeper){
+        this.startNode = startNode
+        this.endNode = endNode
+        this.snapshotKeeper = snapshots
+    }
+
+    fun getNextStep():Snapshot?{
+        currentStep++
+        return snapshotKeeper.getSnapshot(currentStep)
+    }
+
+    fun getPreviousStep():Snapshot?{
+        if (currentStep<=0) return null
+        currentStep--
+        return snapshotKeeper.getSnapshot(currentStep)
+    }
+
+
 }
 
 class MainPresenter(
     val graphView: GraphView
-):ViewObserver {
+):ToolBarView {
 
     private val nodes = ArrayList<UINode>()
     private val edges = ArrayList<UIEdge>()
 
-    private var snapshotKeeper : SnapshotKeeper? = SnapshotKeeper()
+    private val dijkstraAlgorithmController = presenters.DijkstraAlgorithmController()
 
     fun addNode(new:UINode){
         if (new !in nodes)
             nodes.add(new)
     }
+
     fun addEdge(new:UIEdge){
         if (new !in edges)
             edges.add(new)
     }
+
     fun deleteEdge(deleted:UIEdge){
         edges.remove(deleted)
     }
+
     fun deleteNode(deleted:UINode){
         if (deleted in nodes){
             for (e in edges){
@@ -39,24 +70,35 @@ class MainPresenter(
         }
     }
 
-    fun startAlgorithm(){
-        val GRAPH:ArrayList<Edge> = ArrayList<Edge>()
+    fun startAlgorithm(startNode:UINode, endNode:UINode){ //где хранить конечный и начальный узел
+
+        val gr:ArrayList<Edge> = ArrayList<Edge>()
         for (e in edges){
-            //GRAPH.add(Edge(e.sourceNode,e.endNode,e.weight.toInt()))
+            gr.add(Edge(nodes.indexOf(e.sourceNode),nodes.indexOf(e.endNode),e.weight.toInt()))
+        }
+
+        val graph = Graph(gr)
+        graph.dijkstra(nodes.indexOf(startNode)) //прогнали алгоритм
+        dijkstraAlgorithmController.initStart(nodes.indexOf(startNode),nodes.indexOf(endNode),graph.getSnapshotHistory())
+
+    }
+
+    fun nextStep(){
+        val curStep = dijkstraAlgorithmController.getNextStep()
+        for (n in nodes){
+
+        }
+    }
+
+    fun previousStep(){
+        val curStep = dijkstraAlgorithmController.getPreviousStep()
+        for (n in nodes){
+
         }
     }
 
     // принимает какой-то флаг (мб enum)
-    override fun update(){
+    override fun HandleToolBar(){
         TODO("")
     }
-}
-
-enum class Commands() {
-    ADD_NODE,
-    ADD_EDGE,
-    DELETE_NODE,
-    DELETE_EDGE,
-    START_ALGORYTHM
-
 }
